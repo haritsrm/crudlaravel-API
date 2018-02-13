8f7ad7025278230e3b0e9dfa0d133d81c1828724
rietzche <haritzrahman98@gmail.com>
2018-02-13 11:22:38 +0700

    providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+    }
+
+    if ($this->app->environment() == 'production') {
+        URL::forceScheme('https');
     }
 }
